55c25ed26d71ade14f030a41eacc520fc8de3bfb
Ravishankar K N <ravishankarkn2003@gmail.com>
2026-06-15 12:09:20 -0700

    Connect Railway MySQL

--- server/config/db.js
+++ server/config/db.js
@@ -1,25 +1,20 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Abhi",
-  database: "clothing_shop"
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 db.connect((err) => {
-
   if (err) {
-
-    console.log("FULL ERROR:");
-    console.log(err);
-
+    console.log("FULL ERROR:", err);
   } else {
-
     console.log("MySQL Connected");
-
   }
-
 });
 
 module.exports = db;
